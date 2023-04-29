dd16e4ac0e53cc1477bc024ebe2403bedc7ee6d6
Aakash Jangid <aakashg9968@gmail.com>
2023-04-29 11:11:39 +0530

    Array in Name_Class

--- Name.java
+++ Name.java
@@ -1,6 +1,15 @@
+import java.util.*;
+
 public class Name {
     public static void main(String[] args) {
-    System.out.println("This is NAME Class.");
+        Scanner input = new Scanner(System.in);
+        int arr[] = new int[5];
+        System.out.println("This is NAME Class.");
+        System.out.println("Enter the ELements in the Array.");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Arr["+i+"] = ");
+            arr[i] =  input.nextInt();
+        }
     }
     
 }
